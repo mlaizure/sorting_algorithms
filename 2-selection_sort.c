537b23fb92d619ada8430e2c4c53cea7587a75bd
mlaizure <maddi.laizure@gmail.com>
2021-01-29 19:14:02 +0000

    Deleting unnecessary loop for selection sort and adding draft of quick sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,29 +10,21 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0, k = 0, min_idx = 0;
+	size_t i = 0, j = 0, min_idx = 0;
 	int tmp;
 
-	while (1)
+	for (i = 0; i < size - 1; ++i)
 	{
-		for (i = 0; i < size - 1; ++i)
+		min_idx = i;
+		for (j = i + 1; j < size; ++j)
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		if (i != min_idx)
 		{
-			min_idx = i;
-			for (j = i + 1; j < size; ++j)
-				if (array[j] < array[min_idx])
-					min_idx = j;
-			if (i != min_idx)
-			{
-				tmp = array[i];
-				array[i] = array[min_idx];
-				array[min_idx] = tmp;
-				print_array(array, size);
-			}
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
 		}
-		k = 0;
-		while (k < size - 1 && array[k] < array[k + 1])
-			++k;
-		if (k == size - 1)
-			break;
 	}
 }
