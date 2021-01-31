0096e3fdba0d0fd822ec0494ee9f6badff369f94
Aleia DeVore <2496@holbertonschool.com>
2021-01-31 20:53:11 +0000

    Created shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts pairs by interval
+ * @array: array to sort
+ * @size: size of array
+ * Return: void
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t interval = 1, inner, outer;
+	int val;
+
+	if (!array || size < 2)
+		return;
+	while (interval <= size / 3)
+		interval = interval * 3 + 1;
+
+	for (; interval > 0; interval = (interval - 1) / 3)
+	{
+		for (outer = interval; outer < size; outer++)
+		{
+			val = array[outer];
+			inner = outer;
+
+			while (inner > interval - 1 && array[inner - interval] >= val)
+			{
+				array[inner] = array[inner - interval];
+				inner = inner - interval;
+			}
+			array[inner] = val;
+		}
+		print_array(array, size);
+	}
+}
