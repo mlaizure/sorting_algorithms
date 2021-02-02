b30073fe69b0b13fdaea5e8005e73330a501fca5
Aleia DeVore <2496@holbertonschool.com>
2021-02-02 17:42:06 +0000

    Used malloc

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,15 +1,15 @@
 #include "sort.h"
 
 /**
- * get_max - gets max value in array
+ * get_max - gets max value's number of places
  * @a: array
  * @size: size of array
- * Return: largest int
+ * Return: number of places
  */
 
 int get_max(int *a, size_t size)
 {
-	size_t i;
+	size_t i, NOP = 0;
 	int max = a[0];
 
 	for (i = 1; i < size; i++)
@@ -17,7 +17,12 @@ int get_max(int *a, size_t size)
 		if (a[i] > max)
 			max = a[i];
 	}
-	return (max);
+	while (max > 0)
+	{
+		NOP++;
+		max /= 10;
+	}
+	return (NOP);
 }
 
 /**
@@ -29,31 +34,32 @@ int get_max(int *a, size_t size)
 
 void radix_sort(int *array, size_t size)
 {
-	int bucket[30][30], bucket_cnt[30], i, j, k, r, NOP = 0, div = 1, max;
-	int pass, nsize = size;
+	int **bucket, *bucket_cnt, i, j, k, r, NOP = 0, div = 1;
+	int pass, nsize = size, z;
 
 	if (!array || size < 2)
 		return;
-	max = get_max(array, size);
-
-	while (max > 0)
-	{
-		NOP++;
-		max /= 10;
-	}
-
+	NOP = get_max(array, size);
+	if (nsize < NOP)
+		z = NOP;
+	else
+		z = nsize;
+	bucket_cnt = malloc(sizeof(int) * z);
+	bucket = malloc(sizeof(int) * NOP * z);
+	for (i = 0; i < nsize; i++)
+		bucket[i] = malloc(sizeof(int) * NOP);
 	for (pass = 0; pass < NOP; pass++)
 	{
-		for (i = 0; i < 30; i++)
+		for (i = 0; i < z; i++)
 			bucket_cnt[i] = 0;
-		for (i = 0; i < nsize; i++)
+		for (i = 0; i < z; i++)
 		{
 			r = (array[i] / div) % 10;
 			bucket[r][bucket_cnt[r]] = array[i];
 			bucket_cnt[r] += 1;
 		}
 		i = 0;
-		for (k = 0; k < 30; k++)
+		for (k = 0; k < nsize; k++)
 		{
 			for (j = 0; j < bucket_cnt[k]; j++, i++)
 				array[i] = bucket[k][j];
@@ -61,4 +67,8 @@ void radix_sort(int *array, size_t size)
 		div *= 10;
 		print_array(array, size);
 	}
+	free(bucket_cnt);
+	for (i = 0; i < nsize; i++)
+		free(bucket[i]);
+	free(bucket);
 }
